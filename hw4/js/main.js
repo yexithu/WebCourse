055d8671df3d961402552336a553cd05fd0726de
yexithu <502000201@qq.com>
2016-07-08 16:36:42 +0800

    Scroll

--- hw4/js/main.js
+++ hw4/js/main.js
@@ -1,9 +1,11 @@
 var Manager = {
 
 	// constants
-	MININTERVAL: 1200,
+	MININTERVAL: 2000,
 
 	WRAPPERWIDTH: 30,
+
+	SCROLLTIME: 2000,
 
 	TOPPOS: [2, 34, 66],
 
@@ -42,6 +44,8 @@ var Manager = {
 
 	topIndex: 0,
 
+	detailHeight: 0,
+
 	safeTimeout: function (callback, interval) {
 		// if (timerID !== null) {
 		// 	clearTimeout(this.timerID);
@@ -58,6 +62,7 @@ var Manager = {
 	},
 
 	hideNI: function () {
+		// console.log('Hide ' + this.ni());
 		$(this.msgList[this.ni()]).css({'left': '0%', 'top': '2%', 
 		'width' : '0', 'height' : '0', 'padding' : '0', 'opacity' : '0'});
 	},
@@ -80,7 +85,7 @@ var Manager = {
 		if (arguments.length === 1) {
 			$(this.msgList[this.i(arguments[0])]).animate(this.HIDEANIMATE, 
 				this.DURATION);
-			return;
+			return this.DURATION;
 		}
 
 		var i;
@@ -88,13 +93,14 @@ var Manager = {
 			$(this.msgList[this.i(i)]).animate(this.HIDEANIMATE, 
 				this.DURATION);
 		}
+		return this.DURATION;
 	},
 
 	set: function (data) {
 		if (arguments.length === 2) {
-			$(this.nicknameList[this.i(arguments[1])]).html(data.nickname);
-			$(this.contentList[this.i(arguments[1])]).html(data.content);
-			$(this.headimgList[this.i(arguments[1])]).attr('src', data.headimgurl);
+			$(this.nicknameList[arguments[1]]).html(data.nickname);
+			$(this.contentList[arguments[1]]).html(data.content);
+			$(this.headimgList[arguments[1]]).attr('src', data.headimgurl);
 			return;
 		}
 
@@ -110,7 +116,7 @@ var Manager = {
 		if (arguments.length === 1) {
 			$(this.msgList[this.i(arguments[0])]).animate(this.SHOWANIMATE, 
 				this.DURATION);
-			return;
+			return this.DURATION;
 		}
 
 		var i;
@@ -118,24 +124,56 @@ var Manager = {
 			$(this.msgList[this.i(i)]).animate(this.SHOWANIMATE, 
 				this.DURATION);
 		}
+
+		return this.DURATION;
 	},
 
 	onGetHistory: function (data, status) {
-		Manager.hide();
+		var time = 0;
+		time += Manager.hide();
 		Manager.set(data);
-		Manager.show();
+		time += Manager.show();
+		setTimeout(Manager.startScroll, time + 100);
 	},
 
 	showHistory: function () {
 		Backend.fetchHistory(this.onGetHistory);
 	},
 	
-	onTimeOut: function () {
+	scrollCallback: function (i, diff) {
+		return (function () {
+			$(Manager.contentList[Manager.i(i)]).css('top', '0');
+			$(Manager.contentList[Manager.i(i)]).animate({
+				top: '-=' + diff + 'px',
+			}, Manager.SCROLLTIME, Manager.scrollCallback(i, diff));
+		});
+	},
+
+	startScroll: function () {
+		var i = 0;
+		var height = 0;
+		var diff = 0;
+		console.log(Manager.detailHeight);
+		// $(Manager.contentList).css('top', 0);
+		for (i = 0; i < Manager.MAXNUM; ++i) {
+			height = $(Manager.contentList[Manager.i(i)]).height();
+			if (height < Manager.detailHeight) {
+				continue;
+			}
+			diff = (height - Manager.detailHeight);
+			$(Manager.contentList[Manager.i(i)]).animate({
+				top: '-=' + diff + 'px',
+			}, Manager.SCROLLTIME, Manager.scrollCallback(i, diff));
+		}
 
 	},
 
 	updateMessage: function(message) {
 		console.log('UPDATA');
+		$(this.contentList).stop(true, false);
+		$(this.contentList).css('top', '0');
+		// console.log('TOP ' + Manager.topIndex);
+		// console.log('NI ' + Manager.ni());
 		$(this.msgList[this.ni()]).animate({
 			width: '90%',
 			height: '30%',
@@ -150,32 +188,60 @@ var Manager = {
 
 		var i;
 		for (i = 0; i < this.MAXNUM - 1; ++i) {
+			// console.log('This i ' + this.i(i));
 			$(this.msgList[this.i(i)]).animate({
 				top: '+=32%',
 			}, this.DURATION);
 		}
 
+		// console.log('Last ' + this.i(this.MAXNUM - 1));
 		$(this.msgList[this.i(this.MAXNUM - 1)]).animate({
 			width: '0',
 			height: '0',
 			top: '96%',
 			left: '95%',
 			opacity: '0',
-		}, this.DURATION);
+		}, this.DURATION, (function () {
+			Manager.topIndex = (Manager.topIndex + Manager.MAXNUM) % 
+							(Manager.MAXNUM + 1);
+			Manager.hideNI();
+		}));
+
+		// Manager.startScroll();
+		setTimeout((function () { 
+			Manager.startScroll();
+		}), 
+		Manager.DURATION + 100);
+	},
+
+	onTimeOut: function () {
+		if (Manager.msgQueue.length > 0) {
+			var message = Manager.msgQueue.shift();
+			Manager.updateMessage(message);
+			setTimeout(Manager.onTimeOut, Manager.MININTERVAL);
+			return;
+		}
+		Manager.refreshable = true;
 	},
 
 	manageMessage: function (message) {
-		this.msgQueue.push(message);
 		if (this.refreshable) {
 			this.refreshable = false;
-			// setTimeout(Manager.onTimeOut, this.MININTERVAL);
+			setTimeout(Manager.onTimeOut, Manager.MININTERVAL);
 			this.updateMessage(message);
+			return;
 		}
+		this.msgQueue.push(message);
 	},
 
 	onNewMessage: function (message) {
-		console.log(message);
 		Manager.manageMessage(message);
+		
+	},
+
+	initPara: function () {
+		var height = $(this.msgList[0]).height();		
+		this.detailHeight = height * 0.65;
 	},
 
 	init: function () {
@@ -187,11 +253,9 @@ var Manager = {
 		this.headimgList = $('.msg-headimg');
 		//set wrappers to aproximate place
 		this.initWrapperPos();
-
+		this.initPara();
 		this.showHistory();
-
 		Backend.start();
-
 		Backend.setConnectedHandler((function (){ console.log('connected')}));
 		Backend.setNewMessageHandler(this.onNewMessage);
 	},
